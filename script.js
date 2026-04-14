c57c322a4303e17d8626341c51fdca7a3d9079df
shaxzodsniper-lgtm <shaxzod.sniper@gmail.com>
2026-04-14 18:56:29 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+// Bu qatorlarni o'z Supabase ma'lumotlaringiz bilan almashtiring
+const SUPABASE_URL = 'https://vkliiyqxjuhxaafrnyrb.supabase.co'; // Project URL
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZrbGlpeXF4anVoeGFhZnJueXJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxMzkyODYsImV4cCI6MjA5MTcxNTI4Nn0.Jn6xSg8GryDygzB3Y_y99zd1hGJ7ekXZVeNY312GJb8'; // Anon public key
